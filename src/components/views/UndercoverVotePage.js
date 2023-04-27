8ec24eb4304078e78931715d33640a22983ffb69
ruiruiwang <wangruirui45@outlook.com>
2023-04-27 14:07:27 +0200

    solved a display problem

--- src/components/views/UndercoverVotePage.js
+++ src/components/views/UndercoverVotePage.js
@@ -6,22 +6,24 @@ import 'styles/views/UndercoverGame.scss';
 import PropTypes from "prop-types";
 import Room from "../../models/Room";
 import GameUndercover from "../../models/GameUndercover";
+import {getDomain} from "../../helpers/getDomain";
 
 
 const UndercoverVotePage = props => {
-  const history = useHistory();
-  const {gameId} = useParams();
-  const [game, setGame] = useState(null);
-  const [message, setMessage] = useState(null);
-  let [players, setPlayers] = useState(null);
-  const [me, setMe] = useState(null);
+    const history = useHistory();
+    const {gameId} = useParams();
+    const [game, setGame] = useState(null);
+    const [socket, setSocket] = useState(null);
+    const [message, setMessage] = useState('start');
+    let [players, setPlayers] = useState(null);
+    const [me, setMe] = useState(null);
 
 
     useEffect(() => {
         // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
         async function fetchData() {
             try {
-                const response = await api.get('/undercover/'+gameId);
+                const response = await api.get('/undercover/' + gameId);
                 await new Promise(resolve => setTimeout(resolve, 100));
                 console.log(response);
                 // Get the returned users and update the state.
@@ -29,13 +31,13 @@ const UndercoverVotePage = props => {
                 setGame(game);
                 //set the players
                 setPlayers(game.users);
-                players=game.users;
+                players = game.users;
                 console.log(players);
                 // See here to get more data.
                 const updatedPlayers = {};
-                if(players) {
+                if (players) {
                     for (let i = 0; i <= 8; i++) {
-                        if (players[i]&&(players[i].image!=null)) {
+                        if (players[i] && (players[i].image != null)) {
                             const imageResponse = await api.get(`/users/${players[i].id}/image`, {responseType: 'arraybuffer'});
                             players[i].profileImage = `data:image/jpeg;base64,${btoa(new Uint8Array(imageResponse.data).reduce((data, byte) => data + String.fromCharCode(byte), ''))}`;
 
@@ -45,8 +47,7 @@ const UndercoverVotePage = props => {
                     setPlayers(updatedPlayers);
                 }
 
-            }
-            catch (error) {
+            } catch (error) {
                 console.error(`Something went wrong while fetching the game: \n${handleError(error)}`);
                 console.error("Details:", error);
                 alert("Something went wrong while fetching the game! See the console for details.");
@@ -54,7 +55,7 @@ const UndercoverVotePage = props => {
 
             try {
                 const id = localStorage.getItem("id");
-                const response = await api.get('/users/'+id);
+                const response = await api.get('/users/' + id);
                 // Get the returned users and update the state.
                 const me = new User(response.data);
                 //set the local player
@@ -73,37 +74,71 @@ const UndercoverVotePage = props => {
         fetchData();
     }, [gameId]);
 
-    let word,username;
-    if(me){
-        word=me.word;
-        username=me.username;
+    let word, username;
+    if (me) {
+        word = me.word;
+        username = me.username;
     }
     let currentPlayerUsername;
-    if(game){
-        currentPlayerUsername=game.currentPlayerUsername;
+    if (game) {
+        currentPlayerUsername = game.currentPlayerUsername;
     }
 
+    useEffect(() => {
+        const url = "ws" + getDomain().toString().substring(4, getDomain().toString().length) + "/websocket"
+        console.log("ws url");
+        console.log(url);
+        const ws = new WebSocket(url);
+        console.log("ws masssss");
+        console.log(ws);
+        setSocket(ws);
+
+        // 关闭WebSocket连接时清理副作用
+        return () => {
+            ws.close();
+        };
+    }, []);
 
 
     const handleImageClick = async (player) => {
-        try {
-            const response = await api.put('/undercover/' + gameId + '/votes/'+me.id+'/'+player.id);
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            alert("You voted for "+player.username);
-            //check if the game status is voting
-            setGame(response.data);
-            if (game.gameStatus === "describing") {
-                history.push(`/undercover/${gameId}`);
+        if (socket) {
+            try {
+                const response = await api.put('/undercover/' + gameId + '/votes/' + me.id + '/' + player.id);
+                alert("You voted for " + player.username);
+                //check if the game status is voting
+                const gameresponse = await api.get('/undercover/' + gameId);
+                console.log(gameresponse);
+                setGame(gameresponse.data);
+                if (game.gameStatus === "describing") {
+                    history.push(`/undercover/${gameId}`);
+                } else if (game.gameStatus === "gameEnd") {
+                    socket.send(message);
+                    history.push(`/UndercoverGameWinPage`);
+                }
+            } catch (error) {
+                console.error(`Something went wrong while voting: \n${handleError(error)}`);
+                console.error("Details:", error);
+                alert("Something went wrong while voting! See the console for details.");
             }
-            else if (game.gameStatus === "gameEnd") {
-                history.push(`/UndercoverGameWinPage`);
-            }
-        } catch (error) {
-            console.error(`Something went wrong while voting: \n${handleError(error)}`);
-            console.error("Details:", error);
-            alert("Something went wrong while voting! See the console for details.");
         }
     }
+
+    useEffect(() => {
+        if (socket) {
+            socket.onmessage = async (event) => {
+                console.log('WebSocket message received:', event.data);
+                if (event.data === 'start') {
+                    // Check if gameId exists in localStorage
+                    history.push(`/UndercoverGameWinPage`);
+
+                }
+            };
+        }
+    }, [socket, history]);
+
+
+
+
 
     const num = 8;
     const styles = [
@@ -119,7 +154,7 @@ const UndercoverVotePage = props => {
     ]
     const elements = [];
 
-    if(players) {
+    if (players) {
         for (let i = 0; i < 8; i += 2) {
             elements.push(
                 <div>
@@ -146,7 +181,7 @@ const UndercoverVotePage = props => {
                         )}
                         <div className="undercovergame chat-bubble-left">
                             <div style={{"font-weight": "bold", "display": "inline-block"}}>
-                                {players[i] != null ? players[i].username+" : " : ''}
+                                {players[i] != null ? players[i].username + " : " : ''}
                             </div>
                             {players[i] != null ? players[i].description : ''}
                         </div>
@@ -158,7 +193,7 @@ const UndercoverVotePage = props => {
                         "display": "inline-block",
                         "float": "right"
                     }}>
-                        {players[i+1] && (
+                        {players[i + 1] && (
                             <img
                                 style={{
                                     "width": "5.5vw",
@@ -169,16 +204,16 @@ const UndercoverVotePage = props => {
                                     "left": "50%",
                                     "transform": "translate(-50%, -50%)"
                                 }}
-                                src={players[i+1].profileImage}
+                                src={players[i + 1].profileImage}
                                 alt={" "}
-                                onClick={() => handleImageClick(players[i+1])}
+                                onClick={() => handleImageClick(players[i + 1])}
                             />
                         )}
                         <div className="undercovergame chat-bubble-right">
                             <div style={{"font-weight": "bold", "display": "inline-block"}}>
-                                {players[i+1] != null ? players[i+1].username+" : " : ''}
+                                {players[i + 1] != null ? players[i + 1].username + " : " : ''}
                             </div>
-                            {players[i+1] != null ? players[i+1].description : ''}
+                            {players[i + 1] != null ? players[i + 1].description : ''}
                         </div>
                     </div>
                 </div>
@@ -189,13 +224,19 @@ const UndercoverVotePage = props => {
     return (
         <div className="chat-container">
             {elements}
-            <div style={{display: "inline-block",fontWeight: "normal", margin: "-40vw 0 0 5vw", fontSize: "2vw"}}>
+            <div style={{display: "inline-block", fontWeight: "normal", margin: "-40vw 0 0 5vw", fontSize: "2vw"}}>
                 Your Word:
             </div>
-            <div style={{display: "inline-block",fontWeight: "bold", margin: "-40vw 0 0 1vw", fontSize: "2vw", color: "#123597"}}>
+            <div style={{
+                display: "inline-block",
+                fontWeight: "bold",
+                margin: "-40vw 0 0 1vw",
+                fontSize: "2vw",
+                color: "#123597"
+            }}>
                 {word}
             </div>
-            <div style={{display: "inline-block",fontWeight: "normal", margin: "-40vw 0 0 2vw", fontSize: "2vw"}}>
+            <div style={{display: "inline-block", fontWeight: "normal", margin: "-40vw 0 0 2vw", fontSize: "2vw"}}>
                 Click on the profile picture to select who you think is the undercover agent.
             </div>
         </div>
