d2b70157877ff68988e0c695da0f6f33bc4f3006
Jiajian <jiajian.zhu@uzh.ch>
2023-04-27 19:58:21 +0200

    add game history

---
+++ src/models/GameHistory.js
@@ -0,0 +1,17 @@
+/**
+ * Game History model
+ */
+
+
+class GameHistory {
+    constructor(data = {}) {
+        this.id = null;
+        this.username = null;
+        this.winOrLose = null;
+        this.earnedPoint = null;
+        this.gameName = null;
+        this.time = null;
+        Object.assign(this, data);
+    }
+}
+export default GameUndercover;
