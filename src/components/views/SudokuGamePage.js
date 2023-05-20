6418bfb2bdc3bcdcdfcb5f9f270a6b8e1b8386fc
Heqing Ren <heqing.ren@uzh.ch>
2023-05-20 13:41:52 +0200

    fix bug: make the GameSudokuPage successfully open

--- src/components/views/SudokuGamePage.js
+++ src/components/views/SudokuGamePage.js
@@ -6,6 +6,7 @@ import { useHistory, useParams } from 'react-router-dom';
 import "styles/views/GameSudoku.scss";
 
 const SudokuGamePage = () => {
+
   let id = localStorage.getItem('id')
   const history = useHistory();
   const [puzzleArray, setPuzzleArray] = useState([]);
@@ -18,12 +19,8 @@ const SudokuGamePage = () => {
       try {
         const response = await api.get(`/sudoku/create/${requiredDifficulty}`);
         const puzzle = response.data;
-        setPuzzleArray(puzzle.map(row => row.map(cell => cell === "0" ? "" : cell)));
-        console.log("11111111111111111");
-        console.log(puzzleArray);
-        setCopyArray(puzzle.map(row => row.map(cell => cell === "0" ? "" : cell)));
-        console.log("22222222222222222");
-        console.log(copyArray);
+        setPuzzleArray(JSON.parse(JSON.stringify(puzzle)));
+        setCopyArray(JSON.parse(JSON.stringify(puzzle)));
       } catch (error) {
         alert("Something went wrong while fetching the Sudoku Question! See the console for details.");
       }
@@ -33,9 +30,7 @@ const SudokuGamePage = () => {
 
   const submit = async () => {
     const requestBody = puzzleArray
-    const formData = new FormData();
-    formData.append("difficulty", requiredDifficulty);
-    const response2 = await api.put(`/sudoku/validation/${id}`, requestBody, formData);
+    const response2 = await api.put(`/sudoku/validation/${id}/${requiredDifficulty}`, requestBody);
     console.log(response2.data)
     console.log(typeof response2.data === 'boolean')
     if(response2.data === false){
@@ -45,32 +40,34 @@ const SudokuGamePage = () => {
     }
   }
 
-  let content = (
-      puzzleArray.length === 0 ? (
-        <Spinner />
-      ) : (
-        <div className="sudoku-grid" style={{left:'5vw', top:'5vw'}}>
-          {puzzleArray.map((row, rowIndex) => (
-            <div className="row" key={rowIndex}>
-              {row.map((cellValue, colIndex) => (
-                <input
-                  type="text"
-                  className={`cell${copyArray[rowIndex][colIndex] !== "" ? " black-text" : " blue-text"}`}
-                  key={colIndex}
-                  value={cellValue}
-                  readOnly={copyArray[rowIndex][colIndex] !== ""}
-                  onChange={(event) => {
-                    const updatedPuzzleArray = [...puzzleArray];
-                    updatedPuzzleArray[rowIndex][colIndex] = event.target.value;
-                    setPuzzleArray(updatedPuzzleArray);
-                  }}
-                />
-              ))}
-            </div>
-          ))}
-        </div>
-      )
-  );
+  let content = <Spinner />;
+
+  if (puzzleArray.length > 0 && copyArray.length > 0) {
+    content = (
+      <div className="sudoku-grid" style={{ left: '5vw', top: '5vw' }}>
+        {puzzleArray.map((row, rowIndex) => (
+          <div className="row" key={rowIndex}>
+            {row.map((cellValue, colIndex) => (
+              <input
+                type="text"
+                className={`cell${copyArray[rowIndex][colIndex] !== "" ? " black-text" : " blue-text"}`}
+                key={colIndex}
+                value={cellValue}
+                readOnly={copyArray[rowIndex][colIndex] !== ""}
+                onChange={(event) => {
+                  const updatedPuzzleArray = [...puzzleArray];
+                  updatedPuzzleArray[rowIndex][colIndex] = event.target.value;
+                  setPuzzleArray(updatedPuzzleArray);
+                }}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+
 
   return(
     <>
